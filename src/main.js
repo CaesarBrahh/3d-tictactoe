57f536965f27f78f55a6de5ff5dd04e6e1e6dd3b
Julian Ware <julaenw@gmail.com>
2026-03-03 19:32:34 -0600

    initialized renderer

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("hello, world");
+/*
+- Tower of Hanoi type version
+- General 3d version
+*/
+import * as THREE from 'three';
+
+// initialize renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
