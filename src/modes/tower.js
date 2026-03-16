3b93356f518c0cd9bf7ca28329dbf1a0507384ae
Julian Ware <julaenw@gmail.com>
2026-03-15 21:48:43 -0500

    architectural shift

---
+++ src/modes/tower.js
@@ -0,0 +1,8 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { createBoard, createStars } from './board.js';
+import { changeObject } from './logic.js';
+
+export function startTower() {
+	
+}
